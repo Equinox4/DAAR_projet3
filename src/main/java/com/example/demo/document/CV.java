7825afbc810fb37cc78a20eae198e722bd16e596
Equinox4 <equinox4@users.noreply.github.com>
2020-12-05 15:49:04 +0100

    add type annotations + getters and setters

--- src/main/java/com/example/demo/document/CV.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.document;
-
-import org.springframework.data.annotation.Id;
-
-public class CV {
-    @Id private String id;
-    private String firstName;
-    private String lastName;
-}
---
+++ src/main/java/com/example/demo/document/ProfileDocument.java
@@ -0,0 +1,54 @@
+package com.example.demo.documents;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "profiles")
+public class ProfileDocument {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String firstName;
+
+    @Field(type = FieldType.Text)
+    private String lastName;
+
+    @Field(type = FieldType.Text)
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
